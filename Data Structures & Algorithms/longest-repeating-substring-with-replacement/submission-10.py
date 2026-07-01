99fce7556115488f8e2de84e49792de3f08dbf94
yho02 <120296746+yho02@users.noreply.github.com>
2026-06-30 21:12:52 -0700

    Add: longest-repeating-substring-with-replacement - submission-10

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-10.py
@@ -0,0 +1,31 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # caviats: these is purely from memory 
+        # keep the char with the most appearance 
+        # most appearance - k = needed to be replace 
+        # bigger than 0 ok
+        # smaller no ok 
+        # shift the window - one by one i think
+        l = 0 
+        mp = {}
+        res = 1 
+        max_ref = 0 
+        # keep track of the highest character, this changes as the window shifted 
+        # 
+        for r in range (len(s)):
+            if s[r] in mp:
+                mp[s[r]] += 1
+            else:
+                mp[s[r]] = 1
+            if max_ref<  mp[s[r]]:
+                    max_ref =  mp[s[r]]
+            while (r-l+1) - max_ref > k:
+                #total length 
+                print ((r-l+1), max_ref)
+                if mp[s[l]] > 1: 
+                    mp[s[l]] -= 1
+                else: 
+                    mp.pop(s[l], None)        
+                l += 1
+            res = max(res, r-l+1)
+        return res
