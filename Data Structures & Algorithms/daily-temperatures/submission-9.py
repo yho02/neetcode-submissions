65a15ecd5c67a73fbabe118708ae3d39ea4da7c0
yho02 <120296746+yho02@users.noreply.github.com>
2026-07-11 16:04:38 -0700

    Add: daily-temperatures - submission-9

---
+++ Data Structures & Algorithms/daily-temperatures/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0]*len(temperatures)
+        # concern:
+        # 1. where to keep the count? the count 
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                res[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return res 
+                
