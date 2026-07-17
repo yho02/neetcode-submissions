2b7d76c7fbd5283cb57710d39c69831d400ede0b
yho02 <120296746+yho02@users.noreply.github.com>
2026-07-16 20:38:52 -0700

    Add: binary-search - submission-13

---
+++ Data Structures & Algorithms/binary-search/submission-13.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        p = l + ((r - l)//2)
+
+        while l <= r:
+            val = nums[p]
+            if val == target:
+                return p
+            elif val < target:
+                l = p + 1
+            else:
+                r = p - 1
+            p = l + ((r - l)//2)
+        return -1
